ed85b68265cc3b5c5dc2d54cbc40ee91b55b74c4
narin-png <naringul2005@gmail.com>
2024-09-09 14:24:48 +0400

    Add files via upload

---
+++ Task.java
@@ -0,0 +1,29 @@
+package oddevenchecker;
+
+import java.util.Scanner;
+
+
+
+public class Task {
+    public static void main(String[] args) {
+
+
+
+Scanner sc = new Scanner(System.in);
+        System.out.println("enter the first number:");
+        int num1 = sc.nextInt();
+        System.out.println("enter the second  number:");
+        int num2 = sc.nextInt();
+        System.out.println("enter the third number:");
+        int num3 = sc.nextInt();
+        if (num1>num2 &num1>num3){
+            System.out.println("the greatest:"+num1);
+        }
+        else if (num2>num3 &num2>num1){
+            System.out.println("the greatest"+num2);
+        }
+        else
+            System.out.println("the greatest"+num3);
+    }
+}
+
